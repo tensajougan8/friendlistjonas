62bf722ba801a690fea982251606853d46e79f32
mscit9 <mscit9@mscit9-Lenovo-H30-50>
2019-09-24 17:34:17 +0530

    added social features

--- src/javaapplication5/JavaApplication5.java
+++ src/javaapplication5/JavaApplication5.java
@@ -8,13 +8,10 @@ package javaapplication5;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 /**
  *
@@ -22,82 +19,163 @@ import java.util.StringTokenizer;
  */
 public class JavaApplication5 {
 
+    static HashMap<String, LinkedHashSet<String>> map1 = new HashMap<String, LinkedHashSet<String>>();
+    static String currentUserName = "";
+    static LinkedHashSet<String> friendSuggestion = new LinkedHashSet<String>();
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String ar1[];
-        int i = 0, counttrans = 0;
-        ArrayList<String> arr = new ArrayList<String>();
-        ArrayList<String> comb2 = new ArrayList<String>();
-        ArrayList<String> comb3 = new ArrayList<String>();
-        HashMap<String, LinkedHashSet<String>> map1 = new HashMap<String, LinkedHashSet<String>>();
-        LinkedHashMap<String, Integer> map = new LinkedHashMap<String, Integer>();
         try {
-            BufferedReader in = new BufferedReader(
-                    new FileReader("/home/mscit9/d.txt"));
+            Scanner input = new Scanner(System.in);
+            BufferedReader in = new BufferedReader(new FileReader("/home/mscit9/d.txt"));
             String str = in.readLine();
 
-            while ((str = in.readLine()) != null) {
+            while ((str = in.readLine()) != null) {//reading values & inserting them in the map1
                 String[] ar =str.split(",");
-                //StringTokenizer st = new StringTokenizer(str);
+                
+                if(!map1.containsKey(ar[0])){
+                    LinkedHashSet<String> val = new LinkedHashSet<String>();
+                    map1.put(ar[0], val);
+                    val.add(ar[1]);
+                }else{
+                    map1.get(ar[0]).add(ar[1]);
+                }
 
-               
-              //  String ar = st.nextToken();
-            //    st = new StringTokenizer(ar, ",");
-//                while (st.hasMoreTokens()) {
-//                    String key = st.nextToken();
-//                    //System.out.println(item);
-//                    if (map.containsKey(key)) {
-//                        
-//                        map1.get(key).add(key);
-//                        //map1.put(item, );
-//                    } else {
-//                        List<String> friendList = new ArrayList();
-//                        map.put(key, i + 1);
-//                        map1.put(key, 1);
-//                        arr.add(key);
-//                        i++;
-//                    }
-//
-//                }
-            
-            if(!map1.containsKey(ar[0])){
-                LinkedHashSet<String> val = new LinkedHashSet<String>();
-                map1.put(ar[0], val);
-                val.add(ar[1]);
-            }else{
-                map1.get(ar[0]).add(ar[1]);
+                if(!map1.containsKey(ar[1])){
+                    LinkedHashSet<String> val = new LinkedHashSet<String>();
+                    map1.put(ar[1], val);
+                    val.add(ar[0]);
+                }else{
+                    map1.get(ar[1]).add(ar[0]);
+                }
+
             }
-            
-            if(!map1.containsKey(ar[1])){
-                LinkedHashSet<String> val = new LinkedHashSet<String>();
-                map1.put(ar[1], val);
-                val.add(ar[0]);
-            }else{
-                map1.get(ar[1]).add(ar[0]);
+            in.close();
+
+            for(HashMap.Entry<String, LinkedHashSet<String>> entry : map1.entrySet())
+            {
+                String key = entry.getKey();
+                System.out.print(key+" : ");
+                
+                Iterator it = entry.getValue().iterator();
+                while(it.hasNext())
+                    System.out.print(it.next() + " ");
+                System.out.println();
             }
 
+            System.out.println("Enter the yourUsername");
+            currentUserName = input.nextLine();
+
+            FriendSuggestion();
+            
+            System.out.println("Enter the nameOfPerson to stalk");
+            String FriendName = input.nextLine();
+            String chainFriendsString = ChainFriends(FriendName);
+            
+            if(chainFriendsString == null){
+                System.out.println("No Mutual Friends found");
+            } else{
+                String[] splitString = chainFriendsString.split(",");
+                System.out.println("Mutual Friends:");
+                for(int i = 0; i < splitString.length; i++){
+                    System.out.print(splitString[i] + " ");
+                }
+            }
+            
+            System.out.println("Enter the person you hate");
+            String unfriend = input.nextLine();
+            UnFriend(unfriend);
+            
+        } //try block
+        catch (IOException e) {
+            System.out.println("File error");
+        }//catch close
+        
+    }//Closing bracket for main class
+    
+    public static String ChainFriends(String friendName) {
+
+        LinkedHashSet<String> myFriends = null;
+        LinkedHashSet<String> personFriends = null;
+
+        if (map1.containsKey(currentUserName)) {
+            myFriends = map1.get(currentUserName);
         }
-        in.close();
-              
+
+        if (map1.containsKey(friendName)) {
+            personFriends = map1.get(friendName);
+        }
+
+        if (myFriends != null && personFriends != null) {
+            String s = "";
+            Iterator<String> i = myFriends.iterator();
+
+            while (i.hasNext()) {
+                String currentElement = i.next();
+                Iterator<String> j = personFriends.iterator();
+                while (j.hasNext()) {
+                    if (currentElement.equals(j.next())) {
+                        s += currentElement + ",";
+                    }
+                }
+            }
+
+            if (!s.equals("")) {
+                return s.substring(0, s.length() - 1);
+            }
+        }
+
+        return null;
+    }
+    
+    public static void FriendSuggestion(){
+        friendSuggestion.clear();
+        
+        if(map1.containsKey(currentUserName)){
+            LinkedHashSet<String> friendList = map1.get(currentUserName);
+            Iterator<String> friendListit = friendList.iterator();
+            while(friendListit.hasNext())
+            {
+                String friendName = friendListit.next();
+                
+                Iterator<String> friendListit2 = map1.get(friendName).iterator();
+                while(friendListit2.hasNext())
+                {
+                    String friendFriendName = friendListit2.next(); 
+                    if(!friendFriendName.equals(currentUserName) && !friendName.equals(friendFriendName))
+                    {
+                        //remove entry where my name pops up in my friend's friend list
+                        //if friendName is present in both list ie my friend list and my friend's friend list has same person dont include them in final string
+                        friendSuggestion.add(friendFriendName);
+                    }  
+                }
+            }
+        }
+        
+        
+        Iterator it = friendSuggestion.iterator();
+        while(it.hasNext())
+            System.out.print(it.next() + " ");
+        System.out.println();
+    }
+    
+    public static void UnFriend(String unfriend){
+        LinkedHashSet<String> friendListCurrentUser = map1.get(currentUserName);
+        LinkedHashSet<String> friendListUnFriend = map1.get(unfriend);
+        friendListCurrentUser.remove(unfriend);
+        friendListUnFriend.remove(currentUserName);
+        
+        
         for(HashMap.Entry<String, LinkedHashSet<String>> entry : map1.entrySet())
         {
             String key = entry.getKey();
             System.out.print(key+" : ");
-            //LinkedHashSet<String>> friendList = entry.getValue();
-            //for(int i = 0; i < friendList.)
+
             Iterator it = entry.getValue().iterator();
             while(it.hasNext())
                 System.out.print(it.next() + " ");
             System.out.println();
         }
-
-    } //try block
-    catch (IOException e) {
-        System.out.println("File error");
-
-    }//catch close
-
-    }//Closing bracket for main class
+    }
 }
