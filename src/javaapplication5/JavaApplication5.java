5e47612d4d89f477510e9c3180d9151b979c6885
tensajougan8 <52632725+tensajougan8@users.noreply.github.com>
2019-09-24 22:30:36 +0530

    Update JavaApplication5.java

--- src/javaapplication5/JavaApplication5.java
+++ src/javaapplication5/JavaApplication5.java
@@ -33,11 +33,11 @@ public class JavaApplication5 {
 
             while ((str = in.readLine()) != null) {//reading values & inserting them in the map1
                 String[] ar =str.split(",");
-                
+                //inserting the values into the hashmap
                 if(!map1.containsKey(ar[0])){
-                    LinkedHashSet<String> val = new LinkedHashSet<String>();
-                    map1.put(ar[0], val);
-                    val.add(ar[1]);
+                    LinkedHashSet<String> val = new LinkedHashSet<String>();//we use linkedhashset to store it in a array form
+                    map1.put(ar[0], val);//insert the value at index 0 and map it to a hashset
+                    val.add(ar[1]);//adds value to the hashset
                 }else{
                     map1.get(ar[0]).add(ar[1]);
                 }
@@ -53,39 +53,39 @@ public class JavaApplication5 {
             }
             in.close();
 
-            for(HashMap.Entry<String, LinkedHashSet<String>> entry : map1.entrySet())
+            for(HashMap.Entry<String, LinkedHashSet<String>> entry : map1.entrySet())//Prints the hashmap
             {
                 String key = entry.getKey();
                 System.out.print(key+" : ");
                 
-                Iterator it = entry.getValue().iterator();
+                Iterator it = entry.getValue().iterator();//used to iterator to go through the hashset
                 while(it.hasNext())
-                    System.out.print(it.next() + " ");
+                    System.out.print(it.next() + " ");//prints eachh value of the hashset
                 System.out.println();
             }
 
-            System.out.println("Enter the yourUsername");
-            currentUserName = input.nextLine();
+            System.out.println("Enter the yourUsername");//for login purposes
+            currentUserName = input.nextLine();// this name is constant throughout as it is declared as global variable
 
-            FriendSuggestion();
+            FriendSuggestion();//this function suggests the names of friends
             
             System.out.println("Enter the nameOfPerson to stalk");
-            String FriendName = input.nextLine();
-            String chainFriendsString = ChainFriends(FriendName);
+            String FriendName = input.nextLine();//used to take the input name of you want to check the friends list of
+            String chainFriendsString = ChainFriends(FriendName);//stores the friends names that are returned from the function
             
-            if(chainFriendsString == null){
+            if(chainFriendsString == null){//check if the string is null
                 System.out.println("No Mutual Friends found");
             } else{
-                String[] splitString = chainFriendsString.split(",");
+                String[] splitString = chainFriendsString.split(",");//splits the string based on the comma
                 System.out.println("Mutual Friends:");
                 for(int i = 0; i < splitString.length; i++){
-                    System.out.print(splitString[i] + " ");
+                    System.out.print(splitString[i] + " ");//prints the list
                 }
             }
             
             System.out.println("Enter the person you hate");
-            String unfriend = input.nextLine();
-            UnFriend(unfriend);
+            String unfriend = input.nextLine();//input for unfriending
+            UnFriend(unfriend);//function to unfriend
             
         } //try block
         catch (IOException e) {
@@ -100,20 +100,20 @@ public class JavaApplication5 {
         LinkedHashSet<String> personFriends = null;
 
         if (map1.containsKey(currentUserName)) {
-            myFriends = map1.get(currentUserName);
+            myFriends = map1.get(currentUserName);//gets the usernames friends
         }
 
         if (map1.containsKey(friendName)) {
-            personFriends = map1.get(friendName);
+            personFriends = map1.get(friendName);//gets the friends friends list
         }
 
-        if (myFriends != null && personFriends != null) {
+        if (myFriends != null && personFriends != null) { //checks if the any of the list value is empty
             String s = "";
             Iterator<String> i = myFriends.iterator();
 
             while (i.hasNext()) {
                 String currentElement = i.next();
-                Iterator<String> j = personFriends.iterator();
+                Iterator<String> j = personFriends.iterator();//goes through each value 
                 while (j.hasNext()) {
                     if (currentElement.equals(j.next())) {
                         s += currentElement + ",";
